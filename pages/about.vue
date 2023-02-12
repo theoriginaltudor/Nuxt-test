bf10f9a67ddcf233779745aa7c188a4cffc8c208
Tudor Caseru <theoriginaltudor@gmail.com>
2023-02-12 14:52:51 +0100

    learning apis in nuxt

--- pages/about.vue
+++ pages/about.vue
@@ -2,11 +2,17 @@
     <div>
         <h2>About</h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, eaque aliquam quasi voluptatum dolor corporis necessitatibus? Necessitatibus rem labore asperiores et ex quaerat voluptatibus fugiat neque delectus exercitationem, eius adipisci.</p>
+        <div>{{ data }}</div>
     </div>
 </template>
 
 <script setup>
-    
+    const {data} = await useFetch('/api/ninja?name=mario&id=123', {
+        method: 'POST',
+        body: {
+            age: 30
+        }
+    })
 </script>
 
 <style scoped>
